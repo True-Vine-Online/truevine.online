ffe28934a1811b444e897e5582cae4fbcb71fb06
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2024-04-22 14:18:33 -0700

    CADS initial

---
+++ cads/tags/cads1.js
@@ -0,0 +1,3 @@
+function tag1(){
+    document.getElementById("mk4-tag1").innerHTML = '  <center><embed height="350mm" src="https://truevine.online/cads/panel/cads-bar-1.html" width="60%"></center>'
+}
