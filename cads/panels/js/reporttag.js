c9a5ec758f5b343ff513e47f369b306d72b127c0
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2024-04-22 14:22:09 -0700

    fix
    corrected missing tag
    cleaned code

--- cads/panels/js/reporttag.js
+++ cads/panels/js/reporttag.js
@@ -1,11 +1,3 @@
-/*
-function reporttag(){
-    document.getElementById("reportlink").href = 'https://www.borgcreative.com/adreport.html'
-
-    document.getElementById("reportlink1").href = 'https://www.borgcreative.com/adreport.html'
-}
-
-*/
 function reporttag(){
     document.getElementById("reportlink").href = 'https://tally.so/r/wa6PyB'
 
