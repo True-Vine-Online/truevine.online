26bfe312c276aaefad1cd2952c33ea33baad9040
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2024-05-13 12:00:14 -0700

    updated

--- cads/panels/js/reporttag.js
+++ cads/panels/js/reporttag.js
@@ -1,6 +1,6 @@
 function reporttag(){
-    document.getElementById("reportlink").href = 'https://tally.so/r/wa6PyB'
+    document.getElementById("reportlink").href = 'https://tally.so/r/mDVPRZ'
 
-    document.getElementById("reportlink1").href = 'https://tally.so/r/wa6PyB'
+    document.getElementById("reportlink1").href = 'https://tally.so/r/mDVPRZ'
 }
 
